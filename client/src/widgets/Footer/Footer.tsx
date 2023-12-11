bcb2f2b3952972a6fde0d4504ff584a5765b7278
ker4ik13 <kekustop228@mail.ru>
2023-12-11 21:16:20 +0300

    refactor: файлы исправлены с prettier

--- client/src/widgets/Footer/Footer.tsx
+++ client/src/widgets/Footer/Footer.tsx
@@ -1,56 +1,87 @@
-import Logo from '@/shared/Logo/Logo';
-import s from './Footer.module.scss';
-import Link from 'next/link';
+import Logo from "@/shared/Logo/Logo";
+import s from "./Footer.module.scss";
+import Link from "next/link";
 import { FaTelegram, FaWhatsapp } from "react-icons/fa";
-
 
 export const Footer = () => {
   return (
     <div className={s.footer}>
-        <div className={s.container}>
-            <div className={s.containerWrapper}>
-                <div className={s.column}>
-                    <Logo/>
-                    <div className={s.copy}>© 2023 SimbirSoft, ISO 9001:2015. Мы разрабатываем уникальные программные решения для компаний из разных стран.</div>
-                </div>
-                <div className={s.links}>
-                    <div className={s.column}>
-                        <p className={s.linksTitle}>Разделы</p>
-                        <Link href='#' className={s.link}>Главная</Link>
-                        <Link href='#' className={s.link}>О нас</Link>
-                        <Link href='#' className={s.link}>Портфолио</Link>
-                        <Link href='#' className={s.link}>Карта сайта</Link>
-                        <Link href='#' className={s.link}>Контакты</Link>
-                    </div>
-                    <div className={s.column}></div>
-                </div>
-                <div className={s.links}>
-                    <div className={s.column}>
-                        <p className={s.linksTitle}>Разделы</p>
-                        <Link href='#' className={s.link}>SEO продвижение</Link>
-                        <Link href='#' className={s.link}>Контекстная реклама</Link>
-                        <Link href='#' className={s.link}>Создание сайтов</Link>
-                        <Link href='#' className={s.link}>Верстка сайта</Link>
-                        <Link href='#' className={s.link}>Администрирование сайта</Link>
-                    </div>
-                    <div className={s.column}></div>
-                </div>
-                <div className={s.column}>
-                    <p className={s.linksTitle}>Контакты</p>
-                    <Link href="tel:+78124245699" className={`${s.link} ${s.big}`}>+7(812) 424-56-99</Link>
-                    <Link href="tel:+78124245699" className={s.link}>Заказать звонок</Link>
-                    <Link href="mailto:info@simbirsoft.com" className={`${s.link} ${s.big}`}>info@simbirsoft.com</Link>
-                    <div className={s.linksRow}>
-                        <Link href={'#'} className={s.linkIcon}>
-                            <FaTelegram />
-                        </Link>
-                        <Link href={'#'} className={s.linkIcon}>
-                            <FaWhatsapp />
-                        </Link>
-                    </div>
-                </div>
+      <div className={s.container}>
+        <div className={s.containerWrapper}>
+          <div className={s.column}>
+            <Logo />
+            <div className={s.copy}>
+              © 2023 SimbirSoft, ISO 9001:2015. Мы разрабатываем уникальные
+              программные решения для компаний из разных стран.
             </div>
+          </div>
+          <div className={s.links}>
+            <div className={s.column}>
+              <p className={s.linksTitle}>Разделы</p>
+              <Link href='#' className={s.link}>
+                Главная
+              </Link>
+              <Link href='#' className={s.link}>
+                О нас
+              </Link>
+              <Link href='#' className={s.link}>
+                Портфолио
+              </Link>
+              <Link href='#' className={s.link}>
+                Карта сайта
+              </Link>
+              <Link href='#' className={s.link}>
+                Контакты
+              </Link>
+            </div>
+            <div className={s.column}></div>
+          </div>
+          <div className={s.links}>
+            <div className={s.column}>
+              <p className={s.linksTitle}>Разделы</p>
+              <Link href='#' className={s.link}>
+                SEO продвижение
+              </Link>
+              <Link href='#' className={s.link}>
+                Контекстная реклама
+              </Link>
+              <Link href='#' className={s.link}>
+                Создание сайтов
+              </Link>
+              <Link href='#' className={s.link}>
+                Верстка сайта
+              </Link>
+              <Link href='#' className={s.link}>
+                Администрирование сайта
+              </Link>
+            </div>
+            <div className={s.column}></div>
+          </div>
+          <div className={s.column}>
+            <p className={s.linksTitle}>Контакты</p>
+            <Link href='tel:+78124245699' className={`${s.link} ${s.big}`}>
+              +7(812) 424-56-99
+            </Link>
+            <Link href='tel:+78124245699' className={s.link}>
+              Заказать звонок
+            </Link>
+            <Link
+              href='mailto:info@simbirsoft.com'
+              className={`${s.link} ${s.big}`}
+            >
+              info@simbirsoft.com
+            </Link>
+            <div className={s.linksRow}>
+              <Link href={"#"} className={s.linkIcon}>
+                <FaTelegram />
+              </Link>
+              <Link href={"#"} className={s.linkIcon}>
+                <FaWhatsapp />
+              </Link>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- client/src/widgets/Map/Map.tsx
+++ client/src/widgets/Map/Map.tsx
@@ -1,7 +1,16 @@
-import s from './Map.module.scss';
+import s from "./Map.module.scss";
 
 export const Map = () => {
   return (
-    <iframe className={s.iframe} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2334.421478372445!2d45.186204877129526!3d54.190320911241216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4144096e448d8443%3A0xa0b1c7f3eead52ea!2sSimbirSoft!5e0!3m2!1sru!2sru!4v1702275755346!5m2!1sru!2sru" width="600" height="450" style={{border:0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-  )
-}
+    <iframe
+      className={s.iframe}
+      src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2334.421478372445!2d45.186204877129526!3d54.190320911241216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4144096e448d8443%3A0xa0b1c7f3eead52ea!2sSimbirSoft!5e0!3m2!1sru!2sru!4v1702275755346!5m2!1sru!2sru'
+      width='600'
+      height='450'
+      style={{ border: 0 }}
+      allowFullScreen
+      loading='lazy'
+      referrerPolicy='no-referrer-when-downgrade'
+    ></iframe>
+  );
+};
